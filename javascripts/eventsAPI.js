fd5777cbc1be449b2cb4ce4c5c95fd8f89ef6d59
danielcossins11 <daniel.cossins@yahoo.com>
2015-11-16 15:31:12 -0600

    going to make a search by date module

--- javascripts/eventsAPI.js
+++ javascripts/eventsAPI.js
@@ -1,4 +1,4 @@
-define(function(require){
+define(function(require, date){
   var $ = require("jquery");
   var Q = require("q");
   
@@ -6,7 +6,8 @@ define(function(require){
     var deferred = Q.defer();
     
     $.ajax({
-      url: "http://terminal2.expedia.com/x/activities/search?location=Nashville&apikey=OsYfTWKk54EN1WPHxLeAQQu4IZXC5Hwg",
+      // url: "http://terminal2.expedia.com/x/activities/search?location=Nashville&apikey=OsYfTWKk54EN1WPHxLeAQQu4IZXC5Hwg",
+      url: "http://terminal2.expedia.com/x/activities/search?location=Nashville&startDate=2015-11-16&endDate=2015-11-18&apikey=OsYfTWKk54EN1WPHxLeAQQu4IZXC5Hwg",
       method: "GET"
 
       }).done(function(data) {
