a47438b283722533414586a45d3c310283bc38eb
Andri Alexandrou <andri.j.alexandrou@gmail.com>
2015-11-17 14:39:17 -0600

    finished getting user info from account creation

--- javascripts/user-creation.js
+++ javascripts/user-creation.js
@@ -3,36 +3,35 @@ define(function() {
 	
 	createNewAccountButton = $("#submit-created-profile");
 
-	// $("#orientation-input").click(function(e) {
-	// 	selectedOrientation = e.target.firstChild.textContent;
-	// 	console.log("selectedOrientation", selectedOrientation);
+	function getRadioValue(name) {
+	    var value;
+	    // get list of radio buttons with specified name
+		radios = $("input[name = " + name + "]"); 
+	    
+	    // loop through list of radio buttons
+	    for (var i = 0; i < radios.length; i++) {
+	        if ( radios[i].checked ) { // radio checked?
+	            value = radios[i].value; // if so, hold its value in value
+	            break;
+	        }
+	    } // closes for loop
+	    console.log("value", name, value);
+	    return value;
+	} // closes getRadioValue function
 
-	genderOptions = $("#gender-options");
+
+
+	// genderOptions = $("#gender-options");
 
 	createNewAccountButton.click(function () {
 		orientationOptions = $("#orientation-options");
 
-		thisUserObject['username'] = $("#create-username-input").val();
-		thisUserObject['userPassword'] = $("#create-password-input").val();
-		thisUserObject['userAge'] = $("#age-input").val();
+		thisUserObject.username = $("#create-username-input").val();
+		thisUserObject.password = $("#create-password-input").val();
+		thisUserObject.age = $("#age-input").val();
 
-		if (genderOptions === "male") {
-			thisUserObject['userGender'] = "male";
-		} else {
-			thisUserObject['userGender'] = "female";
-		}
-
-		console.log("userOr", orientationOptions);
-		if (orientationOptions === "straight") {
-			console.log("usOer", orientationOptions);
-			thisUserObject['userOrientation'] = "straight";
-		} else if (orientationOptions === "gay") {
-			console.log("usOer", orientationOptions);
-			thisUserObject['userOrientation'] = "gay";
-		} else {
-			console.log("usOer", orientationOptions);
-			thisUserObject['userOrientation'] = "bisexual";
-		} 
+		thisUserObject.gender = getRadioValue("gender");
+		thisUserObject.orientation = getRadioValue("orientation");
 
 		console.log("thisUserObject", thisUserObject);
 		});
